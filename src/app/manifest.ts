49e20e00805648f80fa702253c8612a76dede324
TheSanches <saanches7@gmail.com>
2026-03-12 15:42:03 +0200

    feat: PWA manifest, іконки, заміна img на Image

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'CineUA',
+    short_name: 'CineUA',
+    description: 'Фільми з українським озвученням',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#08080f',
+    theme_color: '#08080f',
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '/icons/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
