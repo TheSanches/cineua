43e098af1415e6cf523f46d936eb3b36b702f5c9
TheSanches <saanches7@gmail.com>
2026-03-13 14:46:28 +0200

    .

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,6 +16,10 @@ export default function LoginPage() {
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/auth/callback`,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
       },
     })
   }
